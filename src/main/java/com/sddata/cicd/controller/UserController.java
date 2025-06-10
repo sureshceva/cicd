d48eb507d5bea28c9cf67841e33ef75191827092
sureshceva <suresh@cevasoftware.com>
2025-06-10 22:29:16 +0530

    first commit for cicd

---
+++ src/main/java/com/sddata/cicd/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.sddata.cicd.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sddata.cicd.model.User;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	@PostMapping("/getall")
+	public List<User> getAll() {
+		List<User> users = List.of(new User(1, "Alice"), new User(2, "Bob"), new User(3, "Charlie"));
+		return users;
+	}
+
+}
---
+++ src/main/java/com/sddata/cicd/model/User.java
@@ -0,0 +1,19 @@
+package com.sddata.cicd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+public class User {
+	
+	private int id;
+	private String name;
+
+}
